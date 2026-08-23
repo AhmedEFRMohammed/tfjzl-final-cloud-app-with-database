cb3d63dce231ca2cb4a0ef63cf1d38ccf5bd7bff
Ahmed E. F. R. Mohammed <91789961+AhmedEFRMohammed@users.noreply.github.com>
2026-08-23 22:24:33 +0300

    made updates to the models.py file, and added the required models.

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,3 +101,31 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=1000)
+    grade = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Question: {self.text}"
+
+    
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, ide__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
